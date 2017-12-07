0d54292a0e3318e36ade9a3b12e405a01e40c008
Christopher Kuemmel <kuemmel.christopher@gmail.com>
2017-12-07 20:19:54 +0100

    update playermovement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour {
     public float distance = 5f;
     public Camera camera;
 	// Use this for initialization
+    bool walk = false;
 	void Start () {
 		
 	}
@@ -14,8 +15,9 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
         // if Controller is touched move in camera direction
-        if (GvrControllerInput.IsTouching) {
-            transform.position = transform.position + Camera.main.transform.forward * distance * Time.deltaTime;
-        }
+        if (walk) transform.position = transform.position + Camera.main.transform.forward * distance * Time.deltaTime;
+
+        if (GvrControllerInput.AppButtonDown) walk = true;
+        else if (GvrControllerInput.AppButtonUp) walk = false;
     }
 }
