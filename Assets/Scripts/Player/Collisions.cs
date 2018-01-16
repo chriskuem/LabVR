823ba3fef7e3b9d197f51015d19fddeb843d24af
Oliver David <me@oliver-david.de>
2018-01-16 18:26:55 +0100

    healthbar + damage from spikes and minotaur; reset to start on death; spikes break on contact, leaving 20 damage

--- Assets/Scripts/Player/Collisions.cs
+++ Assets/Scripts/Player/Collisions.cs
@@ -5,22 +5,43 @@ using UnityEngine;
 public class Collisions : MonoBehaviour {
 
 	Vector3 startPos;
+	Quaternion startRot;
 
 	// Use this for initialization
 	void Start () {
 		startPos = transform.position;
+
+		startRot = Quaternion.LookRotation (new Vector3(0,0,0)-transform.position);
+		transform.rotation = startRot;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		if(PlayerHealth.life<=0){
+			transform.position = startPos;
+			transform.rotation = startRot;
+			PlayerHealth.inFight = false;
+			PlayerHealth.life = 100;
+		}
 	}
 
 	void OnCollisionEnter (Collision col)
 	{
-		if(col.gameObject.name == "Spitze1"||col.gameObject.name == "Spitze2"||col.gameObject.name == "Stab"||col.gameObject.name == "Minotaur")
-		{
-			transform.position=startPos;
+		if (col.gameObject.name == "Spitze1" || col.gameObject.name == "Spitze2" || col.gameObject.name == "Stab") {
+			//schaden durch Fallen
+			PlayerHealth.life = PlayerHealth.life - 20;
+
+			//Zerstöre Falle nach Kollision
+			if (col.gameObject.name == "Stab") {
+				Destroy (col.gameObject);
+			} else {
+				//Zerstöre parent der Spitze --> Stab
+				Destroy (col.gameObject.transform.parent.gameObject);
+			}
+		} 
+		//Start damaging player while in fight (you cant run away from this)
+		else if (col.gameObject.name == "Minotaur") {
+			PlayerHealth.inFight = true;
 		}
 	}
 }
