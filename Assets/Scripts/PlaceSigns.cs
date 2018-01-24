beff0896bbb58a83a7be9d4df8b0fb464e430981
Oliver David <me@oliver-david.de>
2018-01-24 17:01:21 +0100

    add text to sign sockets

---
+++ Assets/Scripts/PlaceSigns.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceSigns : MonoBehaviour {
+
+	List<string> signContents;
+
+	public GameObject signPref;
+
+	// Use this for initialization
+	void Start () {
+		signContents.Add ("Test1");
+		signContents.Add ("Test2");
+		signContents.Add ("Test3");
+		signContents.Add ("Test4");
+		signContents.Add ("Test5");
+
+		foreach (Transform child in gameObject.transform) {
+			var placedSign = Instantiate (signPref, child.position, Quaternion.identity);
+			placedSign.name = child.name+"_text";
+			placedSign.transform.parent = child.gameObject.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
