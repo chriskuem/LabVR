dac6ad61479a4aade066c5813c22bba40baaffd6
Oliver David <me@oliver-david.de>
2018-01-24 16:13:23 +0100

    create sockets for signs

---
+++ Assets/PlaceSigns.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceSigns : MonoBehaviour {
+
+	public int NumberOfSign;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
